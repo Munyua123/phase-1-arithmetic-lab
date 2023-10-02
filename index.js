610d823ea95a4b588054fabb494198720944c35f
Earl <earlmunyua96@gmail.com>
2023-10-02 22:26:24 +0300

    Arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+console.log(multiply)
+
+const random = 1
+Math.random(random)
+
+let num3 = 4000
+let num4 = 1000
+const mod = num3 / num4
+console.log(mod)
+
+const max = 20
+console.log(Math.max(max))
+
+
+
